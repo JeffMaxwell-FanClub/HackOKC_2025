328cb30b1eeb03b20582deb1ae97b659b3ab6eb3
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-11-21 15:56:28 -0600

    Created Basic Routes and Added Express

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/login', (req, res) => {
+  res.send('');
+});
+
+app.get('/signup', (req, res) => {
+  res.send('');
+});
+
+app.get('/user', (req, res) => {
+  res.send('');
+});
+
+app.get('/admin', (req, res) => {
+  res.send('');
+});
